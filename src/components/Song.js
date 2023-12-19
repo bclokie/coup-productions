48c21e656d4f73189a649393efa31810a0a32ec0
Braden <braden.clokie@gmail.com>
2023-12-18 21:08:50 -0500

    Added mute button and volume slider

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,4 +1,4 @@
-// src/components/Song.js
+// Song.js
 import React, { useState, useRef, useEffect } from 'react';
 import WaveSurfer from 'wavesurfer.js';
 import './Song.css';
@@ -7,6 +7,9 @@ const Song = ({ songTitle, albumArtwork, audioSrc }) => {
   const audioRef = useRef(null);
   const waveformRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+  const [volume, setVolume] = useState(1);
+  // eslint-disable-next-line
   const [currentTime, setCurrentTime] = useState(0);
 
   const handlePlayPause = () => {
@@ -16,6 +19,17 @@ const Song = ({ songTitle, albumArtwork, audioSrc }) => {
       audioRef.current.play();
     }
     setIsPlaying((prev) => !prev);
+  };
+
+  const handleMute = () => {
+    audioRef.current.muted = !isMuted;
+    setIsMuted((prev) => !prev);
+  };
+
+  const handleVolumeChange = (e) => {
+    const newVolume = parseFloat(e.target.value);
+    audioRef.current.volume = newVolume;
+    setVolume(newVolume);
   };
 
   const handleTimeUpdate = () => {
@@ -46,6 +60,15 @@ const Song = ({ songTitle, albumArtwork, audioSrc }) => {
       <img src={albumArtwork} alt={`Album Artwork for ${songTitle}`} />
       <div className="audio-controls">
         <button onClick={handlePlayPause}>{isPlaying ? 'Pause' : 'Play'}</button>
+        <button onClick={handleMute}>{isMuted ? 'Unmute' : 'Mute'}</button>
+        <input
+          type="range"
+          min="0"
+          max="1"
+          step="0.1"
+          value={volume}
+          onChange={handleVolumeChange}
+        />
       </div>
       <div className="waveform-container" id="waveform-container"></div>
       <audio
